323bda5a75f0948a9f33a569f515da7c19638071
TheKonamiKoder <thekonamicode02@gmail.com>
2023-08-29 19:52:13 +0100

    Create git repository for project

---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+#define LIGHT_SENSORS_SIZE 3
+
+#define WIFI_CONNECTED D4
+#define LDR_PIN A0
+
+const uint32_t station_id = 0xaf7c1fe6;
+
+const char* ssid = "vodafone041107";
+const char* password = "rpJtaXrLx9cLF6pG";
+
+const char* serve_name = "http://luminocity-server/update_sensor";
+
+const uint8_t light_sensors[LIGHT_SENSORS_SIZE] = {D1, D2, D3};
+
+uint8_t light_intensity;
+
+void setup() 
+{
+	Serial.begin(9600);
+
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+
+	Serial.print("Connecting to "); Serial.println(ssid);
+
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(1000);
+		Serial.print(".");
+	}
+	
+	Serial.println("Connection established!");
+
+	for (auto &&pin : light_sensors)
+	{
+		pinMode(pin, OUTPUT);
+		digitalWrite(pin, LOW);
+	}
+	
+	pinMode(LDR_PIN, INPUT);
+}
+
+long unsigned int last_time = millis();
+
+void loop() 
+{
+	// Uses multiplexing to read the value of multiple sensors from just analog input pin
+	for (auto &&pin : light_sensors)
+	{
+		// Delay added to avoid the  WiFi connection being lost due to overuse of A0
+		if (millis() > (last_time + 1000))
+		{		
+			digitalWrite(pin, HIGH);
+
+			// The A0 pin returns a value from 0 to 1024 as a 10 bit number
+			// Since the luminocity program shows it as a colour, only an 8 bit
+			// number is required to show the light intensity is required
+			light_intensity = (analogRead(LDR_PIN) / 4) - 1;
+
+			digitalWrite(pin, LOW);
+
+			Serial.print("Sensor "); Serial.print(pin); Serial.print(": ");
+			Serial.println(light_intensity);
+
+			String json_data = 
+				"{"
+					"\"station_id\":" + String(station_id) + ","
+					"\"sensor_id\":" + String(pin) + ","
+					"\"type\":" + "0" + ","
+					"\"val\":" + String(light_intensity) + ","
+				"}";
+			
+			Serial.println(json_data);
+
+			last_time = millis();
+
+			// if (WiFi.status() == WL_CONNECTED)
+			// {
+			// 	WiFiClient client;
+			// 	HTTPClient http;
+
+			// 	http.begin(client, serve_name);
+
+			// 	http.addHeader("Content-Type", "application/json");
+
+			// 	http.POST(
+			// 		"{"
+			// 			"\"station_id\":" + String(station_id) + ","
+			// 			"\"sensor_id\":" + String(pin) + ","
+			// 			"\"type\":" + "0" + ","
+			// 			"\"val\":" + String(light_intensity) + ","
+			// 		"}"
+			// 	);
+			// }
+		}		
+	}
+}
+
